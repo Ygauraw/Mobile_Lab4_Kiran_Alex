cedeb82fb514ec4569757a79cc09ab52b09521fd
Alex Wolff <A.Leon.Wolff@gmail.com>
2013-11-13 21:35:17 -0500

    some comments

--- TrackIt/src/com/example/trackit/AppUsageService.java
+++ TrackIt/src/com/example/trackit/AppUsageService.java
@@ -61,34 +61,49 @@ public class AppUsageService extends Service {
      * Updates the SQLite database as to what app is in the foreground
      */
     private void getForegroundApp(){
+    	//create object for app database
     	AppsDataSource appData = new AppsDataSource(this);
+    	//open database
     	appData.open();
+    	//get all apps in database
     	List<AppInfo> allApps = appData.getAllAppss();
+    	//initialize variable for last app that was in foreground
     	AppInfo lastActiveApp = null;
+    	
+    	//initialize variable for current app that is in foreground
     	AppInfo currentActiveApp = null;
+    	//find last app that was in foreground
     	for(AppInfo app : allApps){
     		if(app.getActive()){
     			lastActiveApp = app;
     			break;
     		}
     	}
-    	
+    	//initialize activitymanager variable
     	ActivityManager mActivityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+    	//get running processes
         List<RunningAppProcessInfo> runningAppProcesses = mActivityManager.getRunningAppProcesses();
+        //iterator for running processes
         Iterator<RunningAppProcessInfo> i = runningAppProcesses.iterator();
         while (i.hasNext()) {
+        	//a running process
             RunningAppProcessInfo info = i.next();
-
-            if (info.importance == RunningAppProcessInfo.IMPORTANCE_FOREGROUND) 
-                {
+            //if the current running process is in foreground
+            if (info.importance == RunningAppProcessInfo.IMPORTANCE_FOREGROUND){
+            		//list all package names of foreground process
             		String[] packages = info.pkgList;
+            		//get first package (main package)
             		String foregroundpackage = packages[0];
+            		//get a packagemanager
             		PackageManager pm = this.getPackageManager();
+            		//get date and format
             		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
 					Date date = new Date();
             		try {
+            			//get packageinfo from package name
 						PackageInfo packageinfo = pm.getPackageInfo (foregroundpackage, 0);
 						Boolean exists = false;
+						//see if app is already in database
 						for(AppInfo app : allApps){
 				    		if(app.getPackageInfo().equals(packageinfo)){
 				    			currentActiveApp = app;
@@ -96,16 +111,21 @@ public class AppUsageService extends Service {
 				    			break;
 				    		}
 				    	}
+						//create if new app
 						if(!exists){
 							currentActiveApp = appData.createApp(foregroundpackage, ProdUtils.NO_LABEL, 0, 
 														0, true, dateFormat.format(date));
 						}
+						//check if new foreground app running
 						if(!currentActiveApp.equals(lastActiveApp)){
+							//if last active app exists
 							if(lastActiveApp != null){
 								long newRunTime;
+								//if still from today
 								if(lastActiveApp.getDateRecorded().equals(dateFormat.format(date))){
 									newRunTime = lastActiveApp.getRunTime() + (System.currentTimeMillis() - lastActiveApp.getStartTime());
 								}
+								//calculate time from midnight
 								else{
 									Calendar c = Calendar.getInstance(); //midnight
 								    c.set(Calendar.HOUR_OF_DAY, 0);
@@ -114,10 +134,12 @@ public class AppUsageService extends Service {
 								    c.set(Calendar.MILLISECOND, 0);
 									newRunTime = System.currentTimeMillis() - c.getTimeInMillis();
 								}
+								//replace app data in database
 								appData.deleteApp(lastActiveApp);
 								appData.createApp(lastActiveApp.getPackageInfo().packageName, lastActiveApp.getLabel(), 
 										newRunTime, 0, false, dateFormat.format(date));
 							}
+							//replace app data in database
 							appData.deleteApp(currentActiveApp);
 							appData.createApp(currentActiveApp.getPackageInfo().packageName, currentActiveApp.getLabel(), 
 									currentActiveApp.getRunTime(), System.currentTimeMillis(), true, dateFormat.format(date));
@@ -127,8 +149,11 @@ public class AppUsageService extends Service {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
+            		//break if found foreground app
+            		break;
                }
            }
+        //close database
         appData.close();
     }
 
