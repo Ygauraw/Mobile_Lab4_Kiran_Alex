9b06c46a2ecc0089a1cf3dcf179039a6dffdc6d0
Kiran Vajapey <kvajapey@ucsd.edu>
2013-11-15 12:50:53 -0500

    remove unused layouts

--- TrackIt/gen/com/example/trackit/R.java
+++ TrackIt/gen/com/example/trackit/R.java
@@ -24,23 +24,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
+        public static final int action_settings=0x7f080005;
         public static final int app_list_label_header=0x7f080003;
         public static final int apps_lists=0x7f080004;
         public static final int expandable_Apps_List=0x7f080002;
         public static final int parent_view=0x7f080000;
-        public static final int productive_list_layout=0x7f080005;
         public static final int productivity_score=0x7f080001;
-        public static final int unlabeled_list_layout=0x7f080006;
-        public static final int unproductive_list_layout=0x7f080007;
     }
     public static final class layout {
         public static final int activity_app_list=0x7f030000;
         public static final int app_list_groups=0x7f030001;
         public static final int apps_lists=0x7f030002;
-        public static final int productive_list_layout=0x7f030003;
-        public static final int unlabeled_list_layout=0x7f030004;
-        public static final int unproductive_list_layout=0x7f030005;
     }
     public static final class menu {
         public static final int app_list=0x7f070000;
