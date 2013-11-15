27461e59bb6a4af2ba9e538f772bd3d88a7e14d8
Alex Wolff <A.Leon.Wolff@gmail.com>
2013-11-14 21:24:44 -0500

    database mostly working

--- TrackIt/src/com/example/trackit/AppsDataSource.java
+++ TrackIt/src/com/example/trackit/AppsDataSource.java
@@ -11,6 +11,7 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 public class AppsDataSource {
 
@@ -42,7 +43,7 @@ public class AppsDataSource {
   public AppInfo createApp(String appPackage, String label, long runTime, long startTime, boolean active, String dateRecorded) {
     ContentValues values = new ContentValues();
     values.put(AppOpenHelper.COLUMN_PACKAGE, appPackage);
-    values.put(AppOpenHelper.COLUMN_PACKAGE, label);
+    values.put(AppOpenHelper.COLUMN_LABEL, label);
     values.put(AppOpenHelper.COLUMN_RUNTIME, runTime);
     values.put(AppOpenHelper.COLUMN_STARTTIME, startTime);
     values.put(AppOpenHelper.COLUMN_ACTIVE, String.valueOf(active));
@@ -60,22 +61,24 @@ public class AppsDataSource {
 
   public void deleteApp(AppInfo app) {
     long id = app.getId();
-    System.out.println("App deleted with id: " + id);
     database.delete(AppOpenHelper.APP_TABLE_NAME, AppOpenHelper.COLUMN_ID
         + " = " + id, null);
   }
 
-  public List<AppInfo> getAllAppss() {
+  public List<AppInfo> getAllApps() {
     List<AppInfo> apps = new ArrayList<AppInfo>();
 
     Cursor cursor = database.query(AppOpenHelper.APP_TABLE_NAME,
         allColumns, null, null, null, null, null);
 
-    cursor.moveToFirst();
-    while (!cursor.isAfterLast()) {
-      AppInfo app = cursorToApp(cursor);
-      apps.add(app);
-      cursor.moveToNext();
+    if(cursor.moveToFirst()){
+    	while (!cursor.isAfterLast()) {
+    		AppInfo app = cursorToApp(cursor);
+    		if(app != null){
+    			apps.add(app);
+    		}
+    		cursor.moveToNext();
+    	}
     }
     // make sure to close the cursor
     cursor.close();
@@ -85,21 +88,21 @@ public class AppsDataSource {
   private AppInfo cursorToApp(Cursor cursor) {
 	  PackageManager pm = mContext.getPackageManager();
 	  PackageInfo packageinfo = null;
+	  AppInfo app = null;
 		try {
 			packageinfo = pm.getPackageInfo (cursor.getString(1), 0);
-			//update database
+			app = new AppInfo(packageinfo, mContext);
+			app.setId(cursor.getLong(0));
+			app.setLabel(cursor.getString(2));
+			app.setRunTime(cursor.getLong(3));
+			app.setStartTime(cursor.getLong(4));
+			app.setActive(Boolean.valueOf(cursor.getString(5)));
+			app.setDateRecorded(cursor.getString(6));
 			
 		} catch (NameNotFoundException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//e.printStackTrace();
 		}
-    AppInfo app = new AppInfo(packageinfo, mContext);
-    app.setId(cursor.getLong(0));
-    app.setLabel(cursor.getString(2));
-    app.setRunTime(cursor.getLong(3));
-    app.setStartTime(cursor.getLong(4));
-    app.setActive(Boolean.valueOf(cursor.getString(5)));
-    app.setDateRecorded(cursor.getString(6));
     return app;
   }
 } 
